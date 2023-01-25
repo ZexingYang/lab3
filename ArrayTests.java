a289c6ade77d0311999a9ea2d721995422e6ef24
ZexingYang <zeyang@ucsd.edu>
2023-01-25 12:59:03 -0800

    Change method and tester to debug

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,35 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1, 2, 3, 4};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{4, 3, 2, 1 }, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1,1,2,2 };
+    assertArrayEquals(new int[]{ 2,2,1,1}, ArrayExamples.reversed(input1));
+  }
+  /**
+   * 
+   */
+  @Test
+  public void mytest1() {
+    int[] input= {};
+    assertArrayEquals(new int[0], input);
+  }
+
+  @Test
+  public void mytest2() {
+    int[] input = new int[2];
+    assertArrayEquals(new int[2], input);
+  }
+  @Test 
+  public void testaverage(){
+    double[] input1 = {1,2,3,4};
+    assertEquals((2+3+4)/3.0, ArrayExamples.averageWithoutLowest(input1),0.01); //classname to call
+
   }
 }
